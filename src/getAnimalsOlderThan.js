93be9e5d3c10a9fa0e72f5f72d254d602fa0b833
Felipe Rangel <feliperangel277@gmail.com>
2022-01-25 15:57:13 -0300

    Adiciona requisito 2 - Implementa a funcao getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const speciesList = (specie) => specie.name === animal;
+  const speciesMatch = data.species.find(speciesList);
+  const checkAllAges = (animals) => animals.residents.every((current) => current.age >= age);
+  return checkAllAges(speciesMatch);
 }
 
 module.exports = getAnimalsOlderThan;
