8e1307c6d71e4e8a6fb7e6f9ccacc7a8222a1726
Felipe Rangel <feliperangel277@gmail.com>
2022-01-25 15:36:32 -0300

    Adiciona o requisito 1 - Implementa a funcao getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const speciesList = (specie) => {
+    for (let index = 0; index < ids.length; index += 1) {
+      if (specie.id === ids[index]) {
+        return specie;
+      }
+    }
+  };
+  const speciesMatch = data.species.filter(speciesList);
+  return speciesMatch;
 }
 
 module.exports = getSpeciesByIds;
