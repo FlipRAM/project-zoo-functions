60b98793650bf834253a50f6fd5b14cb83bf7ce0
Felipe Rangel <feliperangel277@gmail.com>
2022-01-26 12:26:03 -0300

    Adiciona o requisito 4 - implementa a funcao getRelatedEmployees

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,16 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const createName = (element) => element.name;
+  const count = (element) => element.residents.length;
+  const obj = data.species.map(createName);
+  const cont = data.species.map(count);
+  const putTogether = (element, index) => element.name 
+  const objFinal = obj.map(putTogether)
+  console.log(obj);
+  console.log(cont);
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,28 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const checkPerson = (person) => person.id === id;
+  const personMatch = data.employees.find(checkPerson);
+  const trueOrFalse = (person) => person.managers.length <= 1;
+  console.log(trueOrFalse(personMatch));
+  return trueOrFalse(personMatch);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const trueOrFalse = isManager(managerId);
+  if (trueOrFalse === true) {
+    const filterPeople = (array) => array.filter(
+      ({ managers }) =>
+        managers[0] === managerId || managers[1] === managerId,
+    );
+    const listOfPeople = filterPeople(data.employees);
+    // const { firstName, lastName } = listOfPeople;
+    const response = listOfPeople.map((element) => `${element.firstName} ${element.lastName}`);
+    return response;
+  } if (trueOrFalse === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
