07035a2036b05a55c174b1a30236b1a27c26bffa
Felipe Rangel <feliperangel277@gmail.com>
2022-01-27 14:47:26 -0300

    Adiciona requisito 9 - implementa a funcao getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const firstAnimal = data.employees.filter((element) => element.id === id)
+    .map((element) => element.responsibleFor[0]);
+  const getAnimal = data.species.filter((element) => element.id === firstAnimal[0]);
+  const getOldestAnimal = getAnimal[0].residents.sort((a, b) => b.age - a.age)[0];
+  const listToReturn = [getOldestAnimal.name, getOldestAnimal.sex, getOldestAnimal.age];
+  return listToReturn;
 }
 
 module.exports = getOldestFromFirstSpecies;
